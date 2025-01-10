e73e78bad1c0f0a6f490baab45e9823c1bcfd779
Dhairya Patel <102957752+dhairya49@users.noreply.github.com>
2025-01-10 11:02:01 +0530

    Update server.js
    changed the file name in line 24

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*',(req,res)=>{res.sendFile(path.join(__dirname, 'build','index.html'))})
+app.get('*',(req,res)=>{res.sendFile(path.join(__dirname, 'build','home.html'))})
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
